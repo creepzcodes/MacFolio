8bd8047b82b688f5274c8901c6c7dd6455f17f9f
Jannik <jannik.reisener@web.de>
2025-12-05 23:21:44 +0100

    implement dock presentation

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -44,7 +44,7 @@ const setupTextHover = (container, type) => {
         })
     }
     const handleMouseLeave = () =>
-        letters.forEach((letter) =>  animateLetter(letter, base, 0.3));
+        letters.forEach((letter) =>  animateLetter(letter, base, 0.5));
 
     container.addEventListener('mousemove', handleMouseMove);
     container.addEventListener('mouseleave', handleMouseLeave);
